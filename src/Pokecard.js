6ee040bbc2e77b01dcf67d9e36f6a57b10a43415
dga80 <dani.dga@gmail.com>
2023-08-30 12:55:47 +0200

    pokecards

---
+++ src/Pokecard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Pokecard.css'
+
+// const POKE_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/"
+let threeDigits = (number) => (number <=999 ? `00${number}`.slice (-3): number)
+
+export function Pokecard(props) {
+    let imgSRC =`${POKE_API}${threeDigits(props.id)}.png`;
+    return (
+        <div className="Pokecard">
+            <h1 className="Pokecard-title">{props.name}</h1>
+            <img src={imgSRC} alt={props.name} />
+            <div className="Pokecard-data">Tydive: {props.type}</div>
+            <div className="Pokecard-data">EXdiv: {props.exp}</div>
+        </div>
+    );
+}
+
